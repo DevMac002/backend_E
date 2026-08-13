f615764e13c91294c0f1a95d2a9a02a6de6c6a10
Esteve abel ezechiel <esteveabelezechiel@192.168.1.87>
2026-08-13 19:08:12 +0000

    fix create group, fix naming

--- src/models/Group.js
+++ src/models/Group.js
@@ -3,7 +3,7 @@ const { sequelize } = require('../config/database');
 
 const Group = sequelize.define('Group', {
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-  nom: { type: DataTypes.STRING(100), allowNull: false },
+  name: { type: DataTypes.STRING(100), allowNull: false },
   description: { type: DataTypes.TEXT, allowNull: true },
   avatar_path: { type: DataTypes.STRING(255), allowNull: true },
   created_by: { type: DataTypes.INTEGER, allowNull: false },
